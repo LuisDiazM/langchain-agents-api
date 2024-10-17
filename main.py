3c13f8ff538774087d35ec06bd13b0457a00cff3
Luis Miguel Diaz Morales <luis.diaz@sofka.com.co>
2024-10-17 09:13:53 -0500

    feat: api with agents include memory

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from typing import List
-from langchain_openai import ChatOpenAI
-from datetime import datetime
-from langchain_core.tools import Tool
-from langchain_core.messages import SystemMessage, AIMessage, ChatMessage
-from langchain import hub
-from langchain.memory import ConversationBufferMemory
-from langchain.agents import AgentExecutor, create_structured_chat_agent
+from infrastructure.cache.chat_history_redis import get_redis
+from langchain_core.messages import AIMessage
+from domain.log.logger import get_logger
+from fastapi import FastAPI
 from wikipedia import summary
-from langchain_redis import RedisChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-import os
+from datetime import datetime
+
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain_core.tools import Tool
+from langchain.agents import AgentExecutor, create_structured_chat_agent
+from langchain.memory import ConversationBufferMemory
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
 def get_current_time(*args, **kwargs):
     """Returns the current time in HH:MM AM/PM format
     """
     now = datetime.now()
     return now.strftime("%I:%M %p")
 
-def search_wikipedia(query:str):
+
+def search_wikipedia(query: str):
     """_summary_
 
     Args:
@@ -27,37 +34,30 @@ def search_wikipedia(query:str):
     except Exception as e:
         return "I couldn't find any information of that"
 
-def tools_generator()->List[Tool]:
+
+def tools_generator() -> List[Tool]:
     return [Tool(name="Time",
-              description="useful when you need to get the current time",
-              func=get_current_time)]
+                 description="useful when you need to get the current time",
+                 func=get_current_time)]
 
 
-def get_redis(session_id:str)->BaseChatMessageHistory:
-    REDIS_URL= os.getenv("REDIS_URL")
-    return RedisChatMessageHistory(session_id, redis_url=REDIS_URL, ttl=3600)
+tools = tools_generator()
+prompt = hub.pull("hwchase17/structured-chat-agent")
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.1)
+agent = create_structured_chat_agent(llm=llm, tools=tools, prompt=prompt)
 
 
-if __name__ == "__main__":
-    tools = tools_generator()
-    prompt = hub.pull("hwchase17/structured-chat-agent")
-    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.1)
+class AgentRequest(BaseModel):
+    session_id: str
+    content: str
 
-    history = get_redis("xxxxxx")
-    memory = ConversationBufferMemory(chat_memory=history, memory_key="chat_history", return_messages=True)
-    agent = create_structured_chat_agent(llm=llm, tools=tools, prompt=prompt)
-
+@app.post("/invoke")
+async def root(request: AgentRequest):
+    history = get_redis(session_id=request.session_id)
+    memory = ConversationBufferMemory(
+        chat_memory=history, memory_key="chat_history", return_messages=True)
     agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools,
-                                                         verbose=True, handle_parsing_errors=True, memory=memory)
-    
-    initial_message = "Eres un asistente virtual que habla español y responderás en ese idiomasu\n "
-    history.add_message(message=SystemMessage(content=initial_message))
-    while True:
-        user_input = input("User: ")
-        if user_input.lower() == "exit":
-            break
-        response = agent_executor.invoke({"input": user_input})
-        bot_message = response.get("output")
-        print(f"Bot: {bot_message}")
-    history.clear()
-
+                                                        handle_parsing_errors=True, memory=memory)
+    response = agent_executor.invoke({"input": request.content})
+    bot_message = response.get("output")
+    return {"response": bot_message, "session_id": request.session_id}
